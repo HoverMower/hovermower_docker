07eaef3d8256ff34a240af72cdcc068c6e90c223
PaddyCube <paddy-daun@web.de>
2023-09-15 13:38:42 +0200

    ublox working

--- bringup_ros.sh
+++ bringup_ros.sh
@@ -6,5 +6,6 @@ docker run -it --net=host \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$HOME/hovermower_overlay_ws:/overlay_ws:rw" \
     --privileged \
+    --publish 4242:4242 \
     ros:hovermower_overlay \
     bash
--- hovermower_overlay/entrypoint.sh
+++ hovermower_overlay/entrypoint.sh
@@ -2,17 +2,15 @@
  
 # Source ROS and Catkin workspaces
 source /opt/ros/noetic/setup.bash
-if [ -f /catkin_ws/devel/setup.bash ]
-then
-  source /catkin_ws/devel/setup.bash
-fi
-if [ -f /overlay_ws/devel/setup.bash ]
-then
-  source /overlay_ws/devel/setup.bash
-fi
+source /catkin_ws/devel/setup.bash
+source /overlay_ws/devel/setup.bash
 echo "Sourced Catkin workspace!"
  
 # Set environment variables
+export ROSMower_USB="/dev/ttyUSB0"
+export HoverBoard_UART="/dev/ttyAMA1"
 
+# Source open_mower config
+source /overlay_ws/src/open_mower_ros/src/open_mower/config/mower_config.sh
 # Execute the command passed into this entrypoint
 exec "$@"
